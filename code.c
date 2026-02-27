6975673e5beed6d369f43b24fe63e192feada932
itracely001 <itracely001@gmail.com>
2026-02-27 20:39:34 +0530

    code.c

---
+++ code.c
@@ -0,0 +1,11 @@
+// Factorial 
+#include <stdio.h>
+int main () {
+    int a , b = 1;
+    printf("Enter your value: ");
+    scanf("%d",&a);
+    for(int c = 1; c <= a ;c ++){
+        b *= c ;
+    }
+    printf("The value of C: %d",b);
+}
